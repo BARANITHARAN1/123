f4111d172025aec25b1a15c900b730e6078ee6fa
BARANITHARAN1 <42020441+BARANITHARAN1@users.noreply.github.com>
2018-08-02 10:21:03 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -4,7 +4,7 @@ class reverse
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.in)
+Scanner s=new Scanner(System.in);
 String s1=s.nextLine();
 String s2= new StringBuffer(s1).reverse().toString();
 System.out.print(s2);
